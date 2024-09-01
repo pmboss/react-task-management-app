58ceba099f70364edc8205a6a63d20aa77e15a77
Pradhumna <pradhumnamalakar@gmail.com>
2024-09-01 20:14:03 +0530

    fix: remove redux-toolkit extension

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import {thunk} from 'redux-thunk';
 import rootReducer from './reducers'; // This is where your root reducer will be imported
 
 // Create the Redux store, applying thunk middleware and dev tools
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  applyMiddleware(thunk)
 );
 
 export default store;
